ffb2eadb12f0a085ef9c5b3fb3ec556d4a3a61e4
Martin Czygan <martin.czygan@gmail.com>
2017-12-16 23:38:03 +0100

    add another meta site

--- contrib/update-sites.sh
+++ contrib/update-sites.sh
@@ -3,3 +3,6 @@
 command -v xmlstarlet >/dev/null 2>&1 || { echo >&2 "xmlstartlet required"; exit 1; }
 command -v curl >/dev/null 2>&1 || { echo >&2 "curl required"; exit 1; }
 curl "http://www.openarchives.org/pmh/registry/ListFriends" | xmlstarlet sel -t -m "/BaseURLs/baseURL/text()" -c . -n - | grep -v '^$$' > sites.tsv
+curl -s http://re.cs.uct.ac.za/cgi-bin/Explorer/2.0-1.47/testoai | grep ^urllist | cut -d ' ' -f 3 | tr -d '";' | grep -v ^$ | sort -u >> sites.tsv
+sort sites.tsv -o sites.tsv
+
