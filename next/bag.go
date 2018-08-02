03b3b72c96b2e8824e067be36d9986ea544c0869
Martin Czygan <martin.czygan@gmail.com>
2018-08-02 15:09:27 +0200

    wip: add notes

--- next/bag.go
+++ next/bag.go
@@ -17,19 +17,22 @@ import (
 // BaseDir for harvests, XXX(miku): use env.
 var BaseDir = filepath.Join(metha.UserHomeDir(), ".methax")
 
+// Harvest contains the basic information on the harvest. Additionally some
+// options.
 type Harvest struct {
-	BaseURL string `json:"baseURL"`
-	From    string `json:"-"`
-	Until   string `json:"-"`
-	Format  string `json:"format"`
-	Set     string `json:"set"`
+	Endpoint string
+	From     string
+	Until    string
+	Format   string
+	Set      string
 
-	Options *HarvestOptions `json:"-"`
+	Options *HarvestOptions
 	cache   struct {
 		identify *metha.Identify
 	}
 }
 
+// HarvestOptions groups options.
 type HarvestOptions struct {
 	MaxRequest                 int
 	DisableSelectiveHarvesting bool
@@ -39,10 +42,20 @@ type HarvestOptions struct {
 	SuppressFormatParameter    bool
 }
 
+// Descriptor describes a harvest. This will be serialized into a file.
+type Descriptor struct {
+	Endpoint  string    `json:"endpoint"`
+	Format    string    `json:"format"`
+	Set       string    `json:"set"`
+	Files     []string  `json:"files"`
+	CreatedAt time.Time `json:"created"`
+	UpdatedAt time.Time `json:"updated"`
+}
+
 // Identify returns the result of an OAI identify request, possibly cached.
 func (h *Harvest) Identify() (*metha.Identify, error) {
 	if h.cache.identify == nil {
-		req := metha.Request{Verb: "Identify", BaseURL: h.BaseURL}
+		req := metha.Request{Verb: "Identify", BaseURL: h.Endpoint}
 		client := metha.CreateClient(30*time.Second, 2)
 		resp, err := client.Do(&req)
 		if err != nil {
@@ -70,7 +83,7 @@ func (h *Harvest) DescriptorPath() string {
 // Directory of the harvest.
 func (h *Harvest) Dir() string {
 	hash := sha1.New()
-	io.WriteString(hash, h.BaseURL)
+	io.WriteString(hash, h.Endpoint)
 	io.WriteString(hash, h.Format)
 	io.WriteString(hash, h.Set)
 	return filepath.Join(BaseDir, fmt.Sprintf("%x", hash.Sum(nil)))
@@ -89,7 +102,7 @@ func (h *Harvest) MkdirAll() error {
 // GranularityToLayout converts the advertised granularity to Go date layout
 // strings. There are two valid values, all else are errors. Granularity
 // controls the kind of request windows possible.
-func (h *Harvest) GranularityToLayout() string {
+func (h *Harvest) GranularityToLayout() (string, error) {
 	idfy, err := h.Identify()
 	if err != nil {
 		return "", err
@@ -114,9 +127,22 @@ func (h *Harvest) WriteDescriptor() error {
 	return ioutil.WriteFile(h.DescriptorPath(), b, 0644)
 }
 
-// XXX(miku): Find last successful date, choose an interval (option, force by
-// error), attempt download (retry, hoops, as one way out of errors, restart
-// with a smaller window size).
+// Files returns the absolute filenames that make up this harvest. XXX(miku):
+// This will be expensive to run on each request.
+func (h *Harvest) Files() []string {
+	return nil
+}
+
+// lastWindow returns the left and right time boundary of the last successful
+// window. Error, if there is none (new harvest).
+func (h *Harvest) lastWindow() (left, right time.Time, err error) {
+	return time.Time{}, time.Time{}, nil
+}
+
+// XXX(miku): Find last successful date (load from about.json, fallback to
+// filesystem), choose an interval (option, force by error), attempt download
+// (retry, hoops, as one way out of errors, restart with a smaller window
+// size).
 func (h *Harvest) Run() error {
 	return nil
 }
