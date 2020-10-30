40e289741819a0173478d9051c4eb69b29ddb3b7
Martin Czygan <martin.czygan@gmail.com>
2020-10-30 23:06:09 +0100

    unique: helper

--- contrib/unique_by_schema.py
+++ contrib/unique_by_schema.py
@@ -6,24 +6,22 @@ Given an list of URL cleanup http, if we have https version as well.
 $ python unique_by_schema.py < file.tsv
 """
 
+import collections
 import fileinput
 import sys
 
 if __name__ == '__main__':
-    http, https = set(), set()
-    for line in fileinput.input():
-        line = line.strip()
-        if line.startswith("http://"):
-            http.add(line[7:])
-        elif line.startswith("https://"):
-            https.add(line[8:])
+    prefix = collections.defaultdict(set)
+    for line in (line.strip() for line in fileinput.input()):
+        if line.startswith("https://"):
+            prefix["https"].add(line[8:])
         else:
-            http.add(line)
+            prefix["http"].add(line[7:])
 
-    for v in http:
-        if v in https:
+    for v in prefix["http"]:
+        if v in prefix["https"]:
             print("dropping: {}".format(v), file=sys.stderr)
             continue
         print("http://{}".format(v))
-    for v in https:
+    for v in prefix["https"]:
         print("https://{}".format(v))
