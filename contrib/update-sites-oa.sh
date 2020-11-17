02ef4a86111f3a8b5e1fb5409448270e3d5097f4
Martin Czygan <martin.czygan@gmail.com>
2020-11-17 01:41:16 +0100

    apply formatting

--- contrib/update-sites-oa.sh
+++ contrib/update-sites-oa.sh
@@ -9,9 +9,14 @@
 set -e
 set -o pipefail
 
-command -v xmlstarlet >/dev/null 2>&1 || { echo >&2 "xmlstarlet required"; exit 1; }
-command -v curl >/dev/null 2>&1 || { echo >&2 "curl required"; exit 1; }
-curl -s "http://www.openarchives.org/pmh/registry/ListFriends" | xmlstarlet sel -t -m "/BaseURLs/baseURL/text()" -c . -n - | grep -v '^$$' > sites-oa.tsv
-curl -s http://re.cs.uct.ac.za/cgi-bin/Explorer/2.0-1.47/testoai | grep ^urllist | cut -d ' ' -f 3 | tr -d '";' | grep -v ^$ | sort -u >> sites-oa.tsv
+command -v xmlstarlet >/dev/null 2>&1 || {
+	echo >&2 "xmlstarlet required"
+	exit 1
+}
+command -v curl >/dev/null 2>&1 || {
+	echo >&2 "curl required"
+	exit 1
+}
+curl -s "http://www.openarchives.org/pmh/registry/ListFriends" | xmlstarlet sel -t -m "/BaseURLs/baseURL/text()" -c . -n - | grep -v '^$$' >sites-oa.tsv
+curl -s http://re.cs.uct.ac.za/cgi-bin/Explorer/2.0-1.47/testoai | grep ^urllist | cut -d ' ' -f 3 | tr -d '";' | grep -v ^$ | sort -u >>sites-oa.tsv
 sort -u sites-oa.tsv -o sites-oa.tsv
-
