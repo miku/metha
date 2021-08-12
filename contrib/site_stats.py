b1872059d5f0aadd40fa473b08222b66b77c60bd
Martin Czygan <martin.czygan@gmail.com>
2021-08-12 14:19:08 +0200

    some more site stats

--- contrib/site_stats.py
+++ contrib/site_stats.py
@@ -4,3 +4,8 @@ pd.set_option('display.max_rows', None)
 
 df = pd.read_json("sites.json", lines=True)
 print(df.groupby("tld").size().sort_values(ascending=False).head(25).to_markdown())
+
+print()
+
+tep = df.groupby(["tld", "is_edu", "platform"]).size()
+print(tep.to_markdown())
