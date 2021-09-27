a92832c877323b0e9dea9fb412f5f97663950f17
Justin Kelly <justin@kelly.org.au>
2021-09-27 15:00:32 +1000

    Encode + in resumptionToken - https://github.com/miku/metha/issues/23

--- request.go
+++ request.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"regexp"
 	"sort"
+	"regexp"
 )
 
 var (
@@ -87,7 +87,7 @@ func (r *Request) URL() (*url.URL, error) {
 		matched, _ := regexp.MatchString(` |\+`, r.ResumptionToken)
 		if matched {
 			// http://opencontext.org/oai/request has spaces in tokens
-			// ExLibris Rosetta has + characters in tokens so encode in
+			// ExLibris Rosetta has + characters in tokens
 			// Encoding in these cases
 			encodedValues = v.Encode()
 		} else {
