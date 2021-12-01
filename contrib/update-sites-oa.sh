51f22f74ebfd90b70ddb82ffe24694d3ffa769d0
Martin Czygan <martin.czygan@gmail.com>
2021-12-01 23:08:24 +0100

    makefile: reduce repetition

--- contrib/update-sites-oa.sh
+++ contrib/update-sites-oa.sh
@@ -9,14 +9,13 @@
 set -e
 set -o pipefail
 
-command -v xmlstarlet >/dev/null 2>&1 || {
-	echo >&2 "xmlstarlet required"
-	exit 1
-}
-command -v curl >/dev/null 2>&1 || {
-	echo >&2 "curl required"
-	exit 1
-}
+for prog in xmlstarlet curl; do
+	command -v $prog >/dev/null 2>&1 || {
+		echo >&2 "$prog required"
+		exit 1
+	}
+done
+
 curl -s "http://www.openarchives.org/pmh/registry/ListFriends" | xmlstarlet sel -t -m "/BaseURLs/baseURL/text()" -c . -n - | grep -v '^$$' >sites-oa.tsv
 curl -s http://re.cs.uct.ac.za/cgi-bin/Explorer/2.0-1.47/testoai | grep ^urllist | cut -d ' ' -f 3 | tr -d '";' | grep -v ^$ | sort -u >>sites-oa.tsv
 sort -u sites-oa.tsv -o sites-oa.tsv
