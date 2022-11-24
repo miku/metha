a59614558d5c33f4c2b4426ab582aedfdc988d2d
Martin Czygan <martin.czygan@gmail.com>
2022-11-24 09:52:27 +0100

    update stats

--- contrib/site_stats.py
+++ contrib/site_stats.py
@@ -7,5 +7,5 @@ print(df.groupby("tld").size().sort_values(ascending=False).head(25).to_markdown
 
 print()
 
-tep = df.groupby(["tld", "is_edu", "platform"], as_index=False).size().sort_values(by="size", ascending=False)
+tep = df.groupby(["tld", "is_edu", "platform"], as_index=False).size().sort_values(by="size", ascending=False).reset_index(drop=True)
 print(tep.to_markdown())
