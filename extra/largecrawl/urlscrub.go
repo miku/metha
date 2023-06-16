e6ea4c88991173453485289e82675ba3ab5bab5c
Martin Czygan <martin.czygan@gmail.com>
2023-06-16 15:28:01 +0200

    update largecrawl notes

--- extra/largecrawl/urlscrub.go
+++ extra/largecrawl/urlscrub.go
@@ -14,19 +14,31 @@ import (
 // url.Parse, Trim                         91672978
 // url.Parse, Trim, Unescape               91629399
 // url.Parse, Trim, Unescape, ReplaceAll   91663562
+//
+// also: limit length
+const minLength = 10
+const maxLength = 120
+
+var replacer = strings.NewReplacer(" ", "", "\t", "")
 
 // SO: 91,663,562 URLs it is
 
 func Scrub(p []byte) ([]byte, error) {
 	s := string(p)
-	s = strings.ReplaceAll(s, " ", "")
 	s = strings.TrimSpace(s)
 	s = html.UnescapeString(s)
+	s = replacer.Replace(s)
+	if len(s) < minLength || len(s) > maxLength {
+		return nil, nil
+	}
 	_, err := url.Parse(s)
 	if err != nil {
 		return nil, nil
 	}
-	return p, nil
+	if !strings.Contains(s, "://") {
+		return nil, nil
+	}
+	return []byte(s + "\n"), nil
 }
 
 func main() {
